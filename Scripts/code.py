8dee6bfd4a89943b613f0af2795f4e528342954c
testsssssssssssss <100393979+testsssssssssssss@users.noreply.github.com>
2022-03-08 04:59:43 -0500

    Update code.py

--- Scripts/code.py
+++ Scripts/code.py
@@ -1,2 +1,8 @@
-sys.stdout.write("Goodbye, World!")
-    
+import datetime
+import time
+
+
+# a job scheduled to run every 10 seconds
+while(True):
+    time.sleep(10)  # 10 seconds.
+    print("farah")  #to print the id of the records added in the collection(for debugging, this value can be observed in the heroku logs)
