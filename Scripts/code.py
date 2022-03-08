7f62194d4c7eea33c201d6efe3f77d0a44a3c5d1
testsssssssssssss <100393979+testsssssssssssss@users.noreply.github.com>
2022-03-08 04:32:58 -0500

    Update code.py

--- Scripts/code.py
+++ Scripts/code.py
@@ -3,7 +3,7 @@ import datetime
 import time
 import smtplib
 
-#while(True):
-print("now changed.. this is a test 5")
+while(True):
+    print("now changed.. this is a test 5")
     #time.sleep(10)  # 5 seconds.
     
