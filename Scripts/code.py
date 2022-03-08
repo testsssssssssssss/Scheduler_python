0ee6fee4ab6a0355f561b8c66f5a36d9c19d6768
testsssssssssssss <100393979+testsssssssssssss@users.noreply.github.com>
2022-03-08 05:14:04 -0500

    Update code.py

--- Scripts/code.py
+++ Scripts/code.py
@@ -1,8 +1,23 @@
+import pymongo
 import datetime
 import time
+from pymongo import MongoClient
 
+
+#For example purpose this code populates a mongodb collection with a dummy data every 10 seconds.
+
+#Connecting to the cluster
+#cluster =MongoClient(<code/credentials to connect to the mongodb remote server>)
+
+#Specifying which DB
+#db=cluster[<name of your database>]    
+
+#Specifying which collection in the DB
+#collection=db[<name of your collection>] 
 
 # a job scheduled to run every 10 seconds
 while(True):
+    #ob=collection.insert_one({'Message':"Scheduler code.py","Time":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+    print("------------------------ Added at : "+datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"------------------------")
     time.sleep(10)  # 10 seconds.
-    print("farah")  #to print the id of the records added in the collection(for debugging, this value can be observed in the heroku logs)
+    print("hi")  #to print the id of the records added in the collection(for debugging, this value can be observed in the heroku logs)
